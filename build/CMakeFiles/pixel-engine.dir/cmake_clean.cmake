90f59ec57b47e0e2644ee195fd63c89c457be770
adrenlabs <pritam007dasp@gmail.com>
2026-07-06 02:37:51 +0530

    Project foundation complete

---
+++ build/CMakeFiles/pixel-engine.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pixel-engine.dir/link.d"
+  "CMakeFiles/pixel-engine.dir/src/FileScanner.cpp.o"
+  "CMakeFiles/pixel-engine.dir/src/FileScanner.cpp.o.d"
+  "CMakeFiles/pixel-engine.dir/src/MetadataReader.cpp.o"
+  "CMakeFiles/pixel-engine.dir/src/MetadataReader.cpp.o.d"
+  "CMakeFiles/pixel-engine.dir/src/main.cpp.o"
+  "CMakeFiles/pixel-engine.dir/src/main.cpp.o.d"
+  "pixel-engine"
+  "pixel-engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pixel-engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
